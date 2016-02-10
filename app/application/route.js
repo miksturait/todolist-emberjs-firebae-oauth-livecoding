0e492aa4ad36949386fc0a22fb915ad16e19fdc7
Michal Czyz <michalczyz@gmail.com>
2016-02-10 19:19:23 +0100

    configuring torii with google auth

--- app/application/route.js
+++ app/application/route.js
@@ -3,5 +3,18 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('todo');
+  },
+
+  beforeModel: function() {
+    return this.get("session").fetch().catch(function() {});
+  },
+
+  actions: {
+    signIn: function(provider = "google") {
+      this.get("session").open("firebase", { provider: provider });
+    },
+    signOut: function() {
+      this.get("session").close();
+    }
   }
 });
