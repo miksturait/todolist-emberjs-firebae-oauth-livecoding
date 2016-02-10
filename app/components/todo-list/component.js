05ec91503b72f32bad976a15a36c440e26916690
Michal Czyz <michalczyz@gmail.com>
2016-02-10 18:29:47 +0100

    todo app with two functionalities * add todo * delete todo

---
+++ app/components/todo-list/component.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  store: Ember.inject.service(),
+  actions: {
+    addTodo() {
+      let description = this.get('description'),
+        store = this.get('store');
+
+      store.createRecord('todo', {description: description});
+
+      this.set('description', '');
+    },
+    deleteTodo(todo) {
+      todo.deleteRecord();
+    }
+  }
+});
